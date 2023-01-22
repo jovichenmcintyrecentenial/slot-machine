6b2dc77bed78a912f9cfc1d377f9e86cd7de1795
jovi848 <j.mcintyre@senit.com>
2023-01-22 14:24:31 -0500

    Initial Commit

---
+++ Slot Machine/GameViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GameViewController.swift
+//  Slot Machine
+//
+//  Created by Jovi on 22/01/2023.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            if let sceneNode = scene.rootNode as! GameScene? {
+                
+                sceneNode.scaleMode = .aspectFill
+                
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    view.ignoresSiblingOrder = true
+                }
+            }
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .portrait
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
