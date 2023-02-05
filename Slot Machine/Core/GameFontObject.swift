1de9fd69f66ced116c6776c9aec90bec3b07e865
jovi848 <j.mcintyre@senit.com>
2023-02-05 18:36:54 -0500

    update comments

--- Slot Machine/Core/GameFontObject.swift
+++ Slot Machine/Core/GameFontObject.swift
@@ -4,6 +4,19 @@
 //
 //  Created by Jovi on 22/01/2023.
 //
+//  GROUP NUMBER: 20
+//  NAMES:
+//  Clinton Nwadiukor  - 301291242
+//  Jovi Chen-Mcintyre - 301125059
+//
+//  DESCRIPTION:
+//  Slot Machine application for iOS. Users can place bets, spin and either win or lose
+//
+//  REVISION HISTORY:
+//  https://github.com/jovichenmcintyrecentenial/slot-machine
+//
+//  DATE LAST MODIFIED:
+//  Feburay 5, 2022
 
 import Foundation
 import SpriteKit
--- Slot Machine/Core/GameObjectProtocol.swift
+++ Slot Machine/Core/GameObjectProtocol.swift
@@ -1,3 +1,16 @@
+//  GROUP NUMBER: 20
+//  NAMES:
+//  Clinton Nwadiukor  - 301291242
+//  Jovi Chen-Mcintyre - 301125059
+//
+//  DESCRIPTION:
+//  Slot Machine application for iOS. Users can place bets, spin and either win or lose
+//
+//  REVISION HISTORY:
+//  https://github.com/jovichenmcintyrecentenial/slot-machine
+//
+//  DATE LAST MODIFIED:
+//  Feburay 5, 2022
 
 protocol GameObjectProtocol{
     func start()
